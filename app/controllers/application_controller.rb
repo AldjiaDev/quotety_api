8c66268957ee05487f9cfef92e7c22fb88c6c272
Aldjia Boughias <boughias-aldjia@live.fr>
2025-05-27 10:54:06 +0200

    Add login feature with session support and dynamic navbar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,28 +1,22 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :null_session
+  protect_from_forgery with: :exception
 
-  before_action :set_current_user_from_token
+  helper_method :current_user
 
   private
 
-  # Décode le token JWT pour récupérer l'utilisateur
-  def set_current_user_from_token
-    header = request.headers['Authorization']
-    token = header&.split(' ')&.last
-
-    if token
-      begin
+  def current_user
+    @current_user ||= begin
+      if session[:user_id]
+        User.find_by(id: session[:user_id])
+      elsif request.headers['Authorization'].present?
+        token = request.headers['Authorization'].split(' ').last
         decoded = JsonWebToken.decode(token)
-        @current_user = User.find_by(id: decoded[:user_id])
-      rescue JWT::DecodeError, ActiveRecord::RecordNotFound
-        @current_user = nil
+        User.find_by(id: decoded[:user_id])
       end
+    rescue
+      nil
     end
   end
-
-  def current_user
-    @current_user
-  end
-
-  helper_method :current_user
 end
+
