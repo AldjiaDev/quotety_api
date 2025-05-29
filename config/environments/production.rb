bc0e314802174b85b81eb1d4d68b68474767a90c
Aldjia Boughias <boughias-aldjia@live.fr>
2025-05-29 17:05:47 +0200

    Fix asset precompilation for Heroku production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,8 +48,10 @@ Rails.application.configure do
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   config.force_ssl = true
-
-  # Skip http-to-https redirect for the default health check endpoint.
+  config.assets.js_compressor = nil
+  config.assets.compile = true
+  config.assets.digest = true
+    # Skip http-to-https redirect for the default health check endpoint.
   # config.ssl_options = { redirect: { exclude: ->(request) { request.path == "/up" } } }
 
   # Log to STDOUT by default
