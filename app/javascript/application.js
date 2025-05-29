9e9f49db739c16c9c1b451590abe5ba394f6e58d
Aldjia Boughias <boughias-aldjia@live.fr>
2025-05-29 17:30:08 +0200

    Configure JS and fix build with esbuild

---
+++ app/javascript/application.js
@@ -0,0 +1,12 @@
+// Import des dépendances Rails de base
+import Rails from "@rails/ujs"
+Rails.start()
+
+// Import de Turbo (si tu utilises Turbo dans Rails 7)
+import "@hotwired/turbo-rails"
+
+// Import des contrôleurs Stimulus si tu en as
+import "controllers"
+
+// Import de Bootstrap (si nécessaire)
+import "bootstrap"
