3a90a4866cec410c8f4f657b5c41fe6396833638
Aldjia Boughias <boughias-aldjia@live.fr>
2025-05-30 21:39:41 +0200

    Ajout UsersController pour l'inscription

--- app/controllers/quotes_web_controller.rb
+++ app/controllers/quotes_web_controller.rb
@@ -3,19 +3,17 @@ class QuotesWebController < ApplicationController
     @quotes = Quote.all
   end
 
-  def category
-    @category = Category.find_by(name: params[:category])
-    if @category
-      @quotes = @category.quotes
-      render :category
-    else
-      redirect_to root_path, alert: "Catégorie introuvable."
-    end
+def category
+  @category = Category.find_by(name: params[:category])
+  if @category
+    @quotes = @category.quotes.includes(:author)
+  else
+    render plain: "Catégorie introuvable : #{params[:category]}", status: :not_found
   end
+end
 
   def search
     query = params[:q]
     @quotes = Quote.where("content ILIKE ?", "%#{query}%")
   end
 end
-
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,14 +7,10 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
-      redirect_to root_path, notice: "Compte créé avec succès !"
+      redirect_to root_path, notice: "Bienvenue sur Quotety !"
     else
-      render :new, status: :unprocessable_entity
+      render :new
     end
-  end
-
-  def show
-    @user = current_user
   end
 
   private
