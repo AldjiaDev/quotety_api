1ed7a6633e1d8162b5eef81831718f952c2efc21
Aldjia Boughias <boughias-aldjia@live.fr>
2025-06-25 11:59:04 +0200

    Homepage card design concistency #27

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -1,10 +1,8 @@
 class PasswordsController < ApplicationController
   def new
-    # affiche le formulaire
   end
 
   def create
-    # Ici tu peux simuler une action, on mettra le vrai envoi d'email plus tard
     flash[:notice] = "Si cette adresse existe, vous recevrez un lien de réinitialisation."
     redirect_to login_path
   end
--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,9 +1,7 @@
 class RegistrationsController < ApplicationController
   def new
-    # Affiche le formulaire d'inscription
   end
 
   def create
-    # Ici aussi on ajoutera la logique pour l’inscription via l’API
   end
 end
